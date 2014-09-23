93360bb83ad9c628d92187d00dafc1edcdb41b94
atell.wu <atell.wu@ndpmedia.com>
2014-09-23 18:42:22 +0800

    init test

---
+++ src/test/java/com/yeahmobi/ut_sample/dao/PersonServiceTest.java
@@ -0,0 +1,29 @@
+package com.yeahmobi.ut_sample.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+public class PersonServiceTest {
+
+//	@Autowired
+//	private PersonMapper personMapper;
+
+	@Test
+	@DatabaseSetup("sampleData.xml")
+	@ExpectedDatabase("expectedData.xml")
+	public void testRemove() throws Exception {
+	}
+
+}
