7a1a2954fecb36e65e2c3477ab92d1ae4c882b6a
atell.wu <atell.wu@ndpmedia.com>
2014-09-24 18:37:53 +0800

    完成dbunit的crud UT例子； todo： Hsqldb的使用

--- src/test/java/com/yeahmobi/ut_sample/dao/PersonServiceTest.java
+++ src/test/java/com/yeahmobi/ut_sample/dao/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package com.yeahmobi.ut_sample.dao;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,19 +12,50 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.yeahmobi.ut_sample.entity.Person;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@ContextConfiguration(locations = { "classpath:applicationContext-test.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DbUnitTestExecutionListener.class })
 public class PersonServiceTest {
 
-//	@Autowired
-//	private PersonMapper personMapper;
+	@Autowired
+	private PersonMapper personMapper;
 
 	@Test
-	@DatabaseSetup("sampleData.xml")
-	@ExpectedDatabase("expectedData.xml")
-	public void testRemove() throws Exception {
+	@DatabaseSetup
+	public void testGet() throws Exception {
+		Person person = personMapper.selectByPrimaryKey(1);
+		Assert.assertEquals("name1", person.getName());
+	}
+
+	@Test
+	@DatabaseSetup
+	@ExpectedDatabase
+	public void testInsert() throws Exception {
+		Person person = new Person();
+		person.setId(3);
+		person.setName("name3");
+		person.setGender("f");
+		personMapper.insert(person);
+	}
+
+	@Test
+	@DatabaseSetup
+	@ExpectedDatabase
+	public void testUpdate() throws Exception {
+		Person person = new Person();
+		person.setId(1);
+		person.setName("name111");
+		personMapper.updateByPrimaryKeySelective(person);
+	}
+
+	@Test
+	@DatabaseSetup
+	@ExpectedDatabase
+	public void testDelete() throws Exception {
+		personMapper.deleteByPrimaryKey(2);
 	}
 
 }
